0f997b39e52890229a31b0d02730d95713a42ac6
Axel Schwenke <axel@mariadb.com>
2022-08-12 11:58:35 +0200

    added: --rand-size to insert the `c` coulmn with variable size during load fix: made the --bulk-load switch working

--- src/lua/oltp_common.lua
+++ src/lua/oltp_common.lua
@@ -80,7 +80,9 @@ sysbench.cmdline.options = {
           "create_secondary is automatically disabled, and " ..
           "delete_inserts is set to 0"},
    bulk_load =
-      {"use extensions for bulk loading, like mysqldump does", false}
+      {"use extensions for bulk loading, like mysqldump does", false},
+   rand_size =
+      {"vary the size of the `c` column this much", 0}
 }
 
 -- Prepare the dataset. This command supports parallel execution, i.e. will
@@ -149,7 +151,15 @@ local pad_value_template = "###########-###########-###########-" ..
    "###########-###########"
 
 function get_c_value()
-   return sysbench.rand.string(c_value_template)
+   local c_value = sysbench.rand.string(c_value_template)
+   local c_len = string.len(c_value)
+
+   if sysbench.opt.rand_size > 0 then
+      c_len = c_len - sysbench.rand.uniform(0, sysbench.opt.rand_size)
+      c_value = string.sub(c_value, 1, c_len)
+   end
+
+   return c_value
 end
 
 function get_pad_value()
@@ -205,8 +215,14 @@ CREATE TABLE sbtest%d(
    con:query(query)
 
    if (sysbench.opt.table_size > 0) then
-      print(string.format("Inserting %d records into 'sbtest%d'",
-                          sysbench.opt.table_size, table_num))
+      if drv:name() == "mysql" and sysbench.opt.bulk_load then
+         con:query("SET autocommit=0, unique_checks=0, foreign_key_checks=0")
+         print(string.format("Inserting %d records into 'sbtest%d' in bulk insert mode",
+                             sysbench.opt.table_size, table_num))
+      else
+         print(string.format("Inserting %d records into 'sbtest%d'",
+                             sysbench.opt.table_size, table_num))
+      end
    end
 
    if sysbench.opt.auto_inc then
@@ -240,6 +256,11 @@ CREATE TABLE sbtest%d(
    end
 
    con:bulk_insert_done()
+
+   if drv:name() == "mysql" and sysbench.opt.bulk_load
+   then
+      con:query("SET autocommit=1, unique_checks=1, foreign_key_checks=1")
+   end
 
    if sysbench.opt.create_secondary then
       print(string.format("Creating a secondary index on 'sbtest%d'...",
@@ -289,11 +310,6 @@ function prepare_commit()
 end
 
 function prepare_for_each_table(key)
-   if drv:name() == "mysql" and sysbench.opt.bulk_load
-   then
-      con:query("SET autocommit=0, unique_checks=0, foreign_key_checks=0")
-   end
-
    for t = 1, sysbench.opt.tables do
       stmt[t][key] = con:prepare(string.format(stmt_defs[key][1], t))
 
@@ -322,11 +338,6 @@ function prepare_for_each_table(key)
       if nparam > 0 then
          stmt[t][key]:bind_param(unpack(param[t][key]))
       end
-   end
-
-   if drv:name() == "mysql" and sysbench.opt.bulk_load
-   then
-      con:query("SET autocommit=1, unique_checks=1, foreign_key_checks=1")
    end
 end
 
